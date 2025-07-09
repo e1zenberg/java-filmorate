abd044340eab4b9bdf91621745be03626f90183e
e1zenberg <e1zenbergdev@gmail.com>
2025-07-09 20:44:02 +0300

    попытка 9

--- src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ValidationException.java
@@ -1,6 +1,12 @@
 package ru.yandex.practicum.filmorate.exception;
 
-// бросается при ошибке валидации
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * Бросается при нарушении бизнес-валидации.
+ */
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ValidationException extends RuntimeException {
     public ValidationException(String message) {
         super(message);
