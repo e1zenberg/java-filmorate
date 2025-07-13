44af42c20a9c2b51febf855d74db409b672f72ae
e1zenberg <e1zenbergdev@gmail.com>
2025-07-13 13:51:25 +0300

    Подавись своими пробелами и душным чекстайлом

--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -1,29 +1,29 @@
 package ru.yandex.practicum.filmorate.storage.user;
 
 import ru.yandex.practicum.filmorate.model.User;
-
 import java.util.Collection;
 import java.util.List;
 
-/**
- * Интерфейс хранилища пользователей:
- * CRUD + дружба (односторонняя).
- */
+// Интерфейс хранилища пользователей: CRUD + дружба (односторонняя).
 public interface UserStorage {
+
     User addUser(User user);
+
     User updateUser(User user);
+
     Collection<User> getAllUsers();
+
     User getUserById(int id);
 
-    /** Добавить в друзья (односторонне) */
+    // Добавить в друзья (односторонне)
     void addFriend(int userId, int friendId);
 
-    /** Убрать из друзей */
+    // Убрать из друзей
     void removeFriend(int userId, int friendId);
 
-    /** Список всех друзей пользователя */
+    // Список всех друзей пользователя
     List<User> getFriends(int userId);
 
-    /** Список общих друзей двух пользователей */
+    // Список общих друзей двух пользователей
     List<User> getCommonFriends(int userId, int otherUserId);
 }
