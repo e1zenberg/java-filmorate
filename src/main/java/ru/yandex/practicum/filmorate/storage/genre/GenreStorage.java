1806a1d568ef8697f48df8bacbebe49605e85c91
e1zenberg <e1zenbergdev@gmail.com>
2025-07-13 13:42:17 +0300

    попытка 6

--- src/main/java/ru/yandex/practicum/filmorate/storage/genre/GenreStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/genre/GenreStorage.java
@@ -8,6 +8,20 @@ import java.util.Collection;
  * Интерфейс хранилища жанров.
  */
 public interface GenreStorage {
+
+    /**
+     * Получить всю коллекцию жанров.
+     *
+     * @return коллекция жанров
+     */
     Collection<Genre> getAllGenres();
+
+    /**
+     * Получить жанр по идентификатору.
+     *
+     * @param id идентификатор жанра
+     * @return объект жанра
+     */
     Genre getGenreById(int id);
+
 }
